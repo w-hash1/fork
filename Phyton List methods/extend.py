0d9653a0f62b643268cd94ee2d911b347f87ab24
Bemnet <bemnet.samuel@aastustudent.edu.et>
2025-04-15 19:09:56 +0300

    today's commit

---
+++ Phyton List methods/extend.py
@@ -0,0 +1,6 @@
+# Using extend with a list
+fruits = ["apple", "banana"]
+more_fruits = ["orange", "grape"]
+fruits.extend(more_fruits)
+print(fruits)  # Output: ['apple', 'banana', 'orange', 'grape']
+
